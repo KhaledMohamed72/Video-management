5f85a18f3928345ff983fdb238027290a0fd2baa
Khaled Mohamed <38916029+KhaledMohamed72@users.noreply.github.com>
2021-02-12 15:14:44 +0200

    Video mangement

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Category;
+use App\Models\Page;
+use App\Models\Skill;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
